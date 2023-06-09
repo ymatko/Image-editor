b2e5bf9485ae40a30bb583c67fa67040bba28af7
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-06-09 20:41:48 +0200

    Add create table histogram 2D with selected values

--- Image editor/Hist2DForm.Designer.cs
+++ Image editor/Hist2DForm.Designer.cs
@@ -28,34 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.BtnNewImage = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.BtnNewImage);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(486, 424);
+            this.splitContainer1.SplitterDistance = 34;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // BtnNewImage
+            // 
+            this.BtnNewImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnNewImage.Location = new System.Drawing.Point(0, 0);
+            this.BtnNewImage.Name = "BtnNewImage";
+            this.BtnNewImage.Size = new System.Drawing.Size(486, 34);
+            this.BtnNewImage.TabIndex = 0;
+            this.BtnNewImage.Text = "Create image";
+            this.BtnNewImage.UseVisualStyleBackColor = true;
+            this.BtnNewImage.Click += new System.EventHandler(this.BtnNewImage_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.RowHeadersWidth = 10;
             this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
-            this.dataGridView1.Size = new System.Drawing.Size(526, 412);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Size = new System.Drawing.Size(486, 386);
+            this.dataGridView1.TabIndex = 2;
             // 
             // Hist2DForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(526, 412);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(486, 424);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "Hist2DForm";
-            this.Text = "Hist2DForm";
+            this.Text = "Histogram 2D";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -63,6 +106,8 @@
 
         #endregion
 
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button BtnNewImage;
         private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Image editor/Hist2DForm.cs
+++ Image editor/Hist2DForm.cs
@@ -25,23 +25,98 @@ namespace Image_editor
                 dataGridView1.Columns.Add($"{i}", $"{i}");
                 dataGridView1.Rows[i].Cells[0].Value = i;
             }
+            for (int i = 0; i < 256; i++)
+            {
+                for(int j = 0; j <= 256; j++)
+                {
+                    if (dataGridView1.Rows[i].Cells[j].Value == null)
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = 0;
+                        dataGridView1.Rows[i].Cells[j].Style.BackColor = Color.LightGray;
+                    }
+                        
+                }
+            }
             for (int x = 0; x < image1.Width; x++)
             {
                 for (int y = 0; y < image1.Height; y++)
                 {
-                    
-                        int value;
-                        if (dataGridView1.Rows[image1.Data[y, x, 0]].Cells[image2.Data[y, x, 0] + 1].Value == null)
-                        {
-                            value = 0;
-                        }
-                        else
-                        {
-                            value = (int)dataGridView1.Rows[image1.Data[y, x, 0]].Cells[image2.Data[y, x, 0] + 1].Value;
-                        }
-                       dataGridView1.Rows[image1.Data[y, x, 0]].Cells[image2.Data[y, x, 0] + 1].Value = value + 1;
-                    
+                    int value;
+                    if (dataGridView1.Rows[image1.Data[y, x, 0]].Cells[image2.Data[y, x, 0] + 1].Value == null)
+                    {
+                        value = 0;
+                    }
+                    else
+                    {
+                        value = (int)dataGridView1.Rows[image1.Data[y, x, 0]].Cells[image2.Data[y, x, 0] + 1].Value;
+                    }
+                    dataGridView1.Rows[image1.Data[y, x, 0]].Cells[image2.Data[y, x, 0] + 1].Value = value + 1;
+                    dataGridView1.Rows[image1.Data[y, x, 0]].Cells[image2.Data[y, x, 0] + 1].Style.BackColor = Color.White;
                 }
+            }
+
+        }
+
+        private void BtnNewImage_Click(object sender, EventArgs e)
+        {
+                CreateNewDataGridViewFromSelection(dataGridView1);
+        }
+        private void CreateNewDataGridViewFromSelection(DataGridView sourceDataGridView)
+        {
+            if (sourceDataGridView.SelectedCells.Count > 0)
+            {
+                DataGridView newDataGridView = new DataGridView();
+
+                #region property of newDataGridView
+                newDataGridView.AllowUserToAddRows = false;
+                newDataGridView.AllowUserToDeleteRows = false;
+                newDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+                newDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+                newDataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+                newDataGridView.Location = new System.Drawing.Point(0, 0);
+                newDataGridView.ReadOnly = true;
+                newDataGridView.RowHeadersVisible = false;
+                newDataGridView.RowHeadersWidth = 10;
+                newDataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+                newDataGridView.Size = new System.Drawing.Size(486, 386);
+                newDataGridView.TabIndex = 2;
+                newDataGridView.AllowUserToResizeColumns = false;
+                newDataGridView.AllowUserToResizeRows = false;
+                #endregion
+                newDataGridView.Columns.Add("number", " ");
+                newDataGridView.Rows.Add(256);
+                for (int i = 0; i < 256; i++)
+                {
+                    newDataGridView.Columns.Add($"{i}", $"{i}");
+                    newDataGridView.Rows[i].Cells[0].Value = i;
+                }
+                for (int i = 0; i < 256; i++)
+                {
+                    for (int j = 0; j <= 256; j++)
+                    {
+                        if (newDataGridView.Rows[i].Cells[j].Value == null)
+                        {
+                            newDataGridView.Rows[i].Cells[j].Value = 0;
+                            newDataGridView.Rows[i].Cells[j].Style.BackColor = Color.LightGray;
+                        }
+
+                    }
+                }
+
+                foreach (DataGridViewCell sourceCell in sourceDataGridView.SelectedCells)
+                {
+                    int rowIndex = sourceCell.RowIndex;
+                    int columnIndex = sourceCell.ColumnIndex;
+                    string cellValue = sourceCell.Value.ToString();
+                    newDataGridView.Rows[rowIndex].Cells[columnIndex].Value = cellValue;
+                    newDataGridView.Rows[rowIndex].Cells[columnIndex].Style.BackColor = Color.White;
+                }
+
+                var form = new Form();
+                form.Controls.Add(newDataGridView);
+                form.Text = "Histogram 2D with selected cells";
+                form.Size = new Size(502, 463);
+                _ = Task.Run(() => form.ShowDialog());
             }
         }
     }
